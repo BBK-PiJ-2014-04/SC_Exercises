f8e41ac8405a890fde28b1fd3b7f364011ba6247
unknown <scarda01@CSPC049.dcs.bbk.ac.uk>
2015-01-26 19:10:42 +0000

    Finally wasn't needed, was actually wrong

--- Exerciseday15/src/exercises/Exercise3.java
+++ Exerciseday15/src/exercises/Exercise3.java
@@ -11,15 +11,14 @@ public class Exercise3 {
 		System.out.println("Please enter 10 number to calculate their mean average");
 		for(int i = 0; i < 10; i++) {
 			Scanner in = new Scanner(System.in);
-		    System.out.println("Enter the " + i + " number");
+		    System.out.println("Enter the " + i + " number:");
 		    try {
 		    	num = Double.parseDouble(in.nextLine());
 		    	sum += num;
 		    } catch (Exception ex) {
-		    	System.out.println("The last input inserted was not a number");
-		    } finally {
+		    	System.out.println("The last input inserted was not a number. Please try again");
 		    	i--;
-		    }
+		    } 
 		}
 		return avg;
 	}
