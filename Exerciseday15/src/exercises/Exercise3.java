9d91a3aa72b77ec06a3b2a6d40c2d7f29b7cb409
unknown <scarda01@CSPC049.dcs.bbk.ac.uk>
2015-01-26 19:45:42 +0000

    Part b implemented and tested

--- Exerciseday15/src/exercises/Exercise3.java
+++ Exerciseday15/src/exercises/Exercise3.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Exercise3 {
 	
-	public static double meanAverage() {
+	public static double meanAverage(int numberOfNumbers) {
 		double sum = 0;
 		Scanner in = new Scanner(System.in);
-		System.out.println("Please enter 10 number to calculate their mean average");
-		for(int i = 1; i < 11; i++) {
+		System.out.println("Please enter " + numberOfNumbers + " number to calculate their mean average");
+		for(int i = 1; i < numberOfNumbers+1; i++) {
 		    System.out.println("Enter the " + i + " number:");
 		    try {
 		    	sum += Double.parseDouble(in.nextLine());
@@ -18,11 +18,18 @@ public class Exercise3 {
 		    }
 		}
 		in.close();
-		return sum/10;
+		return sum/numberOfNumbers;
+	}
+	
+	public static double improvedMeanAverage() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Please enter the number of numbers you want to insert");
+		return meanAverage(Integer.parseInt(in.nextLine()));
 	}
 	
 	public static void main(String [] args)
 	{
-		System.out.println(meanAverage());
+		//System.out.println(meanAverage(10)); //Test for part a
+		System.out.println(improvedMeanAverage()); //Test for part b
 	}
 }
