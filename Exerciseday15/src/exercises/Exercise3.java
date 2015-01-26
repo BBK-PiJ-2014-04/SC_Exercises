5576289d5d7a7d8defbab216bb4efddf6dff5422
unknown <scarda01@CSPC049.dcs.bbk.ac.uk>
2015-01-26 19:16:23 +0000

    Code optimisation

--- Exerciseday15/src/exercises/Exercise3.java
+++ Exerciseday15/src/exercises/Exercise3.java
@@ -5,21 +5,19 @@ import java.util.Scanner;
 public class Exercise3 {
 	
 	public static double meanAverage() {
-		double avg = 0;
-		double num = 0;
 		double sum = 0;
+		Scanner in = new Scanner(System.in);
 		System.out.println("Please enter 10 number to calculate their mean average");
 		for(int i = 1; i < 11; i++) {
-			Scanner in = new Scanner(System.in);
 		    System.out.println("Enter the " + i + " number:");
 		    try {
-		    	num = Double.parseDouble(in.nextLine());
-		    	sum += num;
+		    	sum += Double.parseDouble(in.nextLine());
 		    } catch (Exception ex) {
 		    	System.out.println("The last input inserted was not a number. Please try again");
 		    	i--;
-		    } 
+		    }
 		}
+		in.close();
 		return sum/10;
 	}
 	
