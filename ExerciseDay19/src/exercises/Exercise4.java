af19819efca674889663912d145238c86746bc03
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-02-16 20:06:27 +0000

    Tested, seems to be working

--- ExerciseDay19/src/exercises/Exercise4.java
+++ ExerciseDay19/src/exercises/Exercise4.java
@@ -3,16 +3,23 @@ package exercises;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Exercise4 {
+public class Exercise4 implements Runnable{
 	
-	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static String FinishedTask = "";
+	int milliseconds;
+	int tasknumber;
 	
-	public synchronized int getUserInput(int i) {
+	public Exercise4(int milliseconds) {
+		this.milliseconds = milliseconds;
+	}
+	
+	public synchronized static int getUserInput(int i) {
 		boolean isInt = false;
 		int returnvalue = 0;
 		while(!isInt) {
 			try {
-				System.out.print("Enter the duration (in ms) of task" + i + ":");
+				System.out.print("Enter the duration (in ms) of task " + i + ":");
 				returnvalue = Integer.parseInt(br.readLine());
 				isInt = true;
 			}
@@ -25,7 +32,26 @@ public class Exercise4 {
 	}
 	
 	public static void main(String args[]) {
-		Exercise4 responsiveUI = new Exercise4();
-		int i = responsiveUI.getUserInput(1);
+		for(int n = 0; n <= 10; n++) {
+			Exercise4 responsiveUI = new Exercise4(getUserInput(n));
+			responsiveUI.tasknumber = n;
+			Thread newThread = new Thread(responsiveUI);
+			newThread.start();
+			if(FinishedTask != "") {
+				System.out.println("Finished Tasks:" + FinishedTask);
+				FinishedTask = "";
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(milliseconds);
+			FinishedTask += ((Integer)this.tasknumber).toString() + ", ";
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
