722ea667d646bf9b7aeff92d23d358d4a6a2d90f
BBK-PiJ-2014-04 <salvatore.cardali.dev@gmail.com>
2015-02-23 21:00:30 +0000

    Changes

--- Exerciseday20/src/exercises/SCExecutor.java
+++ Exerciseday20/src/exercises/SCExecutor.java
@@ -1,23 +1,28 @@
 package exercises;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.Executor;
 
 class SCExecutor implements Executor {
+   final List<Runnable> activetasks = Collections.synchronizedList(new ArrayList<Runnable>());
    final Queue<Runnable> waitingtasks = new ArrayDeque<Runnable>();
-   final Queue<Runnable> activetasks = new ArrayDeque<Runnable>();
    int Maxcoresize = 5; //parallel tasks
    Thread monitor = new Thread();
 
    public synchronized void execute(final Runnable r) {
 	   if(activetasks.size() < Maxcoresize) {
-		   activetasks.offer(r);
+		   activetasks.add(r);
 		   new Thread(r).start();
 	   }
 	   else {
 			waitingtasks.offer(r);
 	   }
    }
+   
+   
 
  }
